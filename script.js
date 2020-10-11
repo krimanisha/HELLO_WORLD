9e56cba28c516fb9c3ba9add6512a39f38a5efee
reverk <reverk@users.noreply.github.com>
2020-10-11 14:30:48 +0800

    Moved language code from index.html to config.js

---
+++ script.js
@@ -0,0 +1,32 @@
+let options = {
+    valueNames: [
+        {data: ['lang']},                               // Data attribute
+        'lang-name',                                    // Language name
+        {name: 'lang-image', attr: 'src'},              // Language image
+        {name: 'lang-image-attr', attr: 'alt'},         // Language image alt
+        'contributor',                                  // Github Contributor (name)
+        {name: 'contributor-link', attr: 'href'},       // Github Contributor (link)
+    ],
+    // Render template
+    item: `<div class="col-lg-4 mb-4" data-lang="">
+                <div class="card">
+                    <img class="card-img-top lang-image lang-image-attr"
+                         src=""
+                         alt="" />
+                    <div class="card-body">
+                        <h5 class="card-title text-center lang-name"></h5>
+    
+                        <div class="col text-center">
+                            <a href=""
+                               class="btn btn-outline-danger btn-sm contributor contributor-link"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+};
+
+let list = new List('cards', options, data);
+
+function search(e) {
+    list.search(e.value, ['lang-name'])
+}
